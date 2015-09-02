b0855f92a49761076b3ee47452e762a5a106d559
jarba <bjobard@gmail.com>
2015-09-02 10:05:05 +0200

    First Commit

---
+++ config/development.js
@@ -0,0 +1,19 @@
+{
+	'app':{
+		'name': 'bolgger',
+		'version': '0.0.1',
+		'view':{
+			'engine': 'jade',
+			'pretty'; false
+		}
+	},
+	'db':{
+		'default':{
+			'database': 'mongodb://localhost/db'
+		}
+	},
+	'server':{
+		'http_addr':'localhost',
+		'http_port': 3000
+	}
+}
